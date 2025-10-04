8e2eca1a3ed17b609e4bcf8e8e2ca34333d59b67
Dmitry Blinov <2165514+gurfinkel@users.noreply.github.com>
2025-10-04 14:07:11 +0100

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "faang-jobs-scraper-tfstate"
     key            = "faang-jobs-scraper/terraform.tfstate"
     region         = "us-east-1"
-    encrypt        = true
     use_lockfile   = true
   }
 }
