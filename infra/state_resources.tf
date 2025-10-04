1eeed29692e313f114820008a4ba796aa79f4f2e
Dmitry Blinov <2165514+gurfinkel@users.noreply.github.com>
2025-10-04 14:19:15 +0100

    Update state_resources.tf

--- infra/state_resources.tf
+++ infra/state_resources.tf
@@ -3,13 +3,6 @@ resource "aws_s3_bucket" "tf_state" {
   versioning {
     enabled = true
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
 
 resource "aws_dynamodb_table" "tf_lock" {
